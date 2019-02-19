b5b929f6f443f25dd4ddc3ddbd69026b60ef1f3e
midknighmare <40751627+midknightmare666@users.noreply.github.com>
2019-02-18 20:29:08 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,21 +4,31 @@ let Width = window.innerWidth;
 let Height = window.innerHeight;
 
 let renderer = new THREE.WebGLRenderer();
+  // background color below:
 renderer.setClearColor(0x121212);
+
 renderer.setSize(Width, Height);
 
 let camera = new THREE.PerspectiveCamera(45, Width / Height, 0.01, 10000);
 
 let planeGeometry = new THREE.PlaneGeometry(100, 100, 20, 20);
+  /* wave color below: 
+      wireframe must be set to 'true' otherwise it looks ugly, try it with 'false' lol 
+  */
 let planeMaterial = new THREE.MeshBasicMaterial({color: 0x6D6D6D, wireframe: true});
+
 let plane = new THREE.Mesh(planeGeometry, planeMaterial);
 
 plane.rotation.x = -0.5 * Math.PI;
 
+// plane position duh 
 plane.position.set(0, 0, 0);
 
 scene.add(plane);
 
+/* X, Y, Z positions for camera:
+    anything past 100 is overkill
+*/
 camera.position.set(0, 90, 100);
 camera.lookAt(scene.position);
 
@@ -33,8 +43,12 @@ document.body.appendChild(renderer.domElement);
   for (let i = 0; i < vLength; i++) {
     let v = plane.geometry.vertices[i];
     let dist = new THREE.Vector2(v.x, v.y).sub(center);
-    let size = 6.0;
-    let magnitude = 5;
+    /* size = size of wave
+       magnitude = magnitude of wave 
+    */ 
+    let size = 1.0;
+    let magnitude = 9;
+
     v.z = Math.sin(dist.length()/-size + (ts/500)) * magnitude;
   }
   
